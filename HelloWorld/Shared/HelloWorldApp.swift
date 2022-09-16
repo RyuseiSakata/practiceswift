e03b7cc604fdbd5b7b5694f4028667b124568d06
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2022-09-16 09:56:21 +0900

    アプリ開発
    アプリ開発の練習です

---
+++ HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by 阪田竜生 on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
