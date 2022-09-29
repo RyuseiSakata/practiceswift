1d8931fd5ac012bc41a958ef350e44845ae07308
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2022-09-29 15:09:05 +0900

    カレンダー
    カレンダーを作成できるようになりました

---
+++ timer/timer/Calender.swift
@@ -0,0 +1,12 @@
+//
+//  Calender.swift
+//  timer
+//
+//  Created by 阪田竜生 on 2022/09/29.
+//
+
+import UIKit
+
+class Calender: View {
+
+}
---
+++ timer/timer/FSCalendre.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import FSCalendar
+ 
+struct CalendarTestView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIView {
+        
+        typealias UIViewType = FSCalendar
+        
+        let fsCalendar = FSCalendar()
+            
+        return fsCalendar
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+    }
+}
+ 
